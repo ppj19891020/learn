31716fbc7013fa4c7ea01fc6cc0a670791ee1ee5
pangpeijie <pangpeijie@dxy.cn>
2020-07-08 19:53:32 +0800

    [add]stack

--- src/main/java/com/fly/learn/algorithm/linkedlist/反转链表_206.java
+++ src/main/java/com/fly/learn/algorithm/linkedlist/反转链表_206.java
@@ -1,16 +1,6 @@
 package com.fly.learn.algorithm.linkedlist;
 
 /**
- //给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
- //
- // 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
- //
- //
- //
- // 示例:
- //
- // 给定 1->2->3->4, 你应该返回 2->1->4->3.
- //
  // Related Topics 链表 * leetcode题目：https://leetcode-cn.com/problems/reverse-linked-list/
  * 反转一个单链表。
  * 示例:
