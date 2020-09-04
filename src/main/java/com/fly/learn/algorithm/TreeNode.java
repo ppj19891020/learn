e7b1280243204ce56a9e27ce54332dcb6605e1ed
pangpeijie <pangpeijie@dxy.cn>
2020-09-04 19:36:06 +0800

    [add]二叉树的所有路径

---
+++ src/main/java/com/fly/learn/algorithm/TreeNode.java
@@ -0,0 +1,14 @@
+package com.fly.learn.algorithm;
+
+/**
+ * @author: peijiepang
+ * @date 2020/9/4
+ * @Description:
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int x) { val = x; }
+
+}
