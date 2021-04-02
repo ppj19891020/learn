962349f061d3ad9cfbce261f2ee321fef002f7e0
pangpeijie <pangpeijie@dxy.cn>
2021-04-02 18:04:17 +0800

    [add]leetcode

---
+++ src/main/java/com/fly/learn/algorithmV2/二叉树/TreeNode.java
@@ -0,0 +1,20 @@
+package com.fly.learn.algorithmV2.二叉树;
+
+/**
+ * @author: peijiepang
+ * @date 2021/4/2
+ * @Description:
+ */
+public class TreeNode {
+
+    int val;
+
+    TreeNode left;
+
+    TreeNode right;
+
+     TreeNode(int x) {
+         val = x;
+     }
+
+}
